6ff30f604bf3edd719764bc35b78cb82fb9c65d0
Margarita <margarita.petrova.kn@gmail.com>
2021-12-23 00:19:02 +0200

    Added Error page

---
+++ src/components/Error/Error.js
@@ -0,0 +1,14 @@
+import './Error.css'
+const Error = () => {
+  return (
+    <section className="error-page">
+
+      <img src="https://cdn.shopify.com/s/files/1/1048/8552/products/groot_sitting1_large.png?v=1570299830" alt=""/>
+      <h1>
+        Page Not Found
+      </h1>
+    </section>
+  );
+}
+
+export default Error;

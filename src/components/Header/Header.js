114645dcae9c214c17bf48fede74a790316541ee
Margarita <margarita.petrova.kn@gmail.com>
2021-12-23 00:15:49 +0200

    Modified Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,26 +3,30 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext'
 
 const Header = () => {
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     let guestNavigation = (
         <div id="guest">
-            <Link to="/login" href="/login">Login</Link>
-            <Link to="/register" href="/register">Register</Link>
+            <Link to="/login" href="/login" className="cloud-link">Login</Link>
+            <Link to="/register" href="/register" className="cloud-link">Register</Link>
         </div>
     );
     let userNavigation = (
         <div id="user">
-            <span>Hello, {user.email}</span>
-            <Link to="/create" href="/create">Create Hero</Link>
-            <Link to="/logout" href="/logout">Logout</Link>
+            <Link to="/create" href="/create" className="cloud-link">Create</Link>
+            <Link to="/logout" href="/logout" className="cloud-link">Logout</Link>
         </div>
 
     );
     return (
         <header>
-            <h1><Link to="/" className="home" href="/">ComicsWorld</Link></h1>
+            <Link to="/" className="home" href="/">
+                <img className="logo" src="/images/logo.png" alt="ComicsWorld" />
+            </Link>
+
             <nav>
-                <Link to="/catalog" href="/catalog" className="cloud-link">All heroes</Link>
+                <div id="all">
+                    <Link to="/catalog" href="/catalog" className="cloud-link">All heroes</Link>
+                </div>
                 {user.email
                     ? userNavigation
                     : guestNavigation

e7c2bd80ee9e539ed0447aed3c4694e3fb6ff59b
Margarita <margarita.petrova.kn@gmail.com>
2021-12-27 18:00:00 +0200

    Created bulgarian labels constants

---
+++ client/src/common/labelsConstatnsBG.js
@@ -0,0 +1,29 @@
+export const navigationTitlesBG = {
+    'AllHeroes': 'Всички герои',
+    'Create': 'Създай',
+    'MyHeroes': 'Моите герои',
+    'Login':'Вход',
+    'Register':'Регистрация',
+    'Logout':'Изход',
+}
+
+export const formLabelsBG = {
+    'PersonName':'Рождено име',
+    'HeroicName':'Име на супергероя',
+    'Age':'Години',
+    'Image':'Скица на героя',
+    'Story':'История на героя',
+}
+
+export const buttonLabelsBG = {
+    'Create': 'Създай',
+    'Edit': 'Редактирай',
+    'Cancel': 'Отказ',
+}
+
+export const placeholdersBG = {
+    'PersonName': '',
+    'HeroicName': '',
+    'Image': '',
+    'Story': '',
+}
---
+++ client/src/common/messagesConstantsBG.js
@@ -0,0 +1,13 @@
+export const titles = {
+    'AllSuperheroes': 'Всички създадени герои',
+    'TopHeroes': 'Най-харесвани',
+    'Create': 'Създаване на герой',
+    'Edit': 'Редактиране на герой',
+
+}
+
+export const alertMessages = {
+    'NoSuperheroes': 'Все още няма създадени герои',
+    'SomethingWentWrong': 'Нещо се обърка...',
+
+}

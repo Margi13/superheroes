8c9a78a4cf9f3be39deb5cc99373941b404b99ee
Margarita <margarita.petrova.kn@gmail.com>
2021-12-27 18:00:40 +0200

    Created Header css file and updated navigation UI

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,31 +1,35 @@
 import { Link } from 'react-router-dom';
-import { useAuthContext } from '../../contexts/AuthContext'
+import { useAuthContext } from '../../contexts/AuthContext';
+import { navigationTitlesBG } from '../../common/labelsConstatnsBG';
+import './Header.css';
 
 const Header = () => {
     const { user } = useAuthContext();
     let guestNavigation = (
-        <div id="guest">
-            <Link to="/login" href="/login" className="cloud-link">Login</Link>
-            <Link to="/register" href="/register" className="cloud-link">Register</Link>
+        <div id="guest" className="nav-container">
+            <Link to="/login" href="/login" className="cloud-link guest-nav">{navigationTitlesBG.Login}</Link>
+            <Link to="/register" href="/register" className="cloud-link guest-nav">{navigationTitlesBG.Register}</Link>
         </div>
     );
     let userNavigation = (
-        <div id="user">
-            <Link to="/create" href="/create" className="cloud-link">Create</Link>
-            <Link to="/my-heroes" href="/my-heroes" className="cloud-link">My Heroes</Link>
-            <Link to="/logout" href="/logout" className="cloud-link">Logout</Link>
+        <div id="user" className="nav-container">
+            <Link to="/create" href="/create" className="cloud-link user-nav">{navigationTitlesBG.Create}</Link>
+            <Link to="/my-heroes" href="/my-heroes" className="cloud-link user-nav">{navigationTitlesBG.MyHeroes}</Link>
+            <Link to="/logout" href="/logout" className="cloud-link user-nav">{navigationTitlesBG.Logout}</Link>
         </div>
 
     );
     return (
         <header>
-            <Link to="/" className="home" href="/">
-                <img className="logo" src="/images/logo.png" alt="ComicsWorld" />
-            </Link>
+            <div className='logo-container'>
+                <Link to="/" className="home" href="/" >
+                    <img className="logo" src="/images/logo1.png" alt="BGComicsWorld" />
+                </Link>
+            </div>
 
             <nav>
-                <div id="all">
-                    <Link to="/catalog" href="/catalog" className="cloud-link">All heroes</Link>
+                <div id="all" className="nav-container">
+                    <Link to="/catalog" href="/catalog" className="cloud-link all-nav">{navigationTitlesBG.AllHeroes}</Link>
                 </div>
                 {user.email
                     ? userNavigation

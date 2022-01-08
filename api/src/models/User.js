de0a38a3ad0f842280be09703e7e9baf22f41139
Margarita <margarita.petrova.kn@gmail.com>
2022-01-08 16:37:24 +0200

    Add model for User with mongoose

---
+++ api/src/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userShema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User', userShema);
+
+module.exports = User

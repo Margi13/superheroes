96123833a869ea26b97f7a7b0d497d68231b007f
Margarita <margarita.petrova.kn@gmail.com>
2022-01-08 16:33:15 +0200

    Add new custom server with express.js

---
+++ api/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/superheroes')
+    .then(() => {
+        console.log('DB Connected');
+    });
+mongoose.connection.on('error', (error) => {
+    console.log('DB Error:', error);
+})
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({ text: 'It\'s working!' });
+});
+
+app.use(routes);
+
+app.listen(5000, () => console.log('App is running on port 5000'))

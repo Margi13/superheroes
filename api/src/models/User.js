609eb3210efa1e8510b761d1415a26ffa8147865
Margarita <margarita.petrova.kn@gmail.com>
2022-01-10 00:54:20 +0200

    Add role to user

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 
 const userShema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    role: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 const User = mongoose.model('User', userShema);
+//TODO: Bicrypt for password
 
 module.exports = User

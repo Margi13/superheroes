7aec8bd9c3c66c6cc5ac2c8e2afa210e5cac728b
Margarita <margarita.petrova.kn@gmail.com>
2022-01-10 00:54:00 +0200

    Add functinoality for creating admin

--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -1,14 +1,32 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const jwt = require('jsonwebtoken')
-exports.register = ({email, password})=>User.create({email, password});
+const roleService = require('./roleService');
 
-exports.login = async ({email, password}) =>{
-    let user = await User.findOne({email});
-    if(user){
-        let token = jwt.sign({_id: user._id, email: user.email}, 'SECRETTOKEN');
+const ADMIN_EMAIL = 'admin@abv.bg';
+const ADMIN_PASSWORD = 'admin';
 
-        return {user, token};
-    }else{
+exports.createAdmin = async () => {
+    const roleId = roleService.getRoleIdByName(roleService.ADMIN_ROLE);
+    const result = await User.create({ ADMIN_EMAIL, ADMIN_PASSWORD, roleId });
+    if (result) {
+        return result;
+    } else {
+        throw new Error('Cannot register admin user')
+    }
+};
+
+exports.register = async ({ email, password }) => {
+    const roleId = roleService.getRoleIdByName(roleService.USER_ROLE);
+    return await User.create({ email, password, roleId });
+};
+
+exports.login = async ({ email, password }) => {
+    let user = await User.findOne({ email });
+    if (user) {
+        let token = jwt.sign({ _id: user._id, email: user.email }, 'SECRETTOKEN');
+
+        return { user, token };
+    } else {
         throw new Error('No such user')
     }
 }

d6b3c85329bea7874e7161ea6d4ec10d8bd540d3
Margarita <margarita.petrova.kn@gmail.com>
2022-07-02 11:28:48 +0300

    Fixed server

--- api/src/server.js
+++ api/src/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 const routes = require('./routes');
-const config = require('./config/config.json')[process.env.NODE_ENV];
-const initDatabase = requirer('./config/database');
+const config = require('./config/config');
+const initDatabase = require('./config/database');
 const { auth } = require('./middlewares/authMiddleware');
 const { initializeData } = require('./initialize');
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -16,9 +17,9 @@ app.use(auth);
 // app.use(express.static(path.resolve(__dirname, './public')))
 app.use(routes);
 
-initDatabase(config.DB_CONNECTION_STRING)
+initDatabase(config.db.DB_CONNECTION_STRING)
     .then(() => {
-        app.listen(config.PORT, () => console.log(`App is running on http://localhost:${config.PORT}`));
+        app.listen(config.db.PORT, () => console.log(`App is running on http://localhost:${config.db.PORT}`));
     })
     .catch(err => {
         console.log('Application init failed: ', err)
